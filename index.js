7a71c36cb35938fad668ee363c5be418cb56522a
Brian Woodward <brian.woodward@gmail.com>
2015-03-13 09:46:26 -0400

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * helper-async <https://github.com/doowb/helper-async>
+ *
+ * Copyright (c) 2015 Brian Woodward.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
