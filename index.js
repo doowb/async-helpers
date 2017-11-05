bfad3039d55f5e35f7a3f29786db06642e26e223
jonschlinkert <jon.schlinkert@sellside.com>
2017-11-05 02:13:52 -0500

    revert `isHelperGroup` change
    since it's counting for "more than one" not "one or more"

--- index.js
+++ index.js
@@ -531,13 +531,10 @@ function isHelperGroup(helpers) {
     return true;
   }
   if (typeof helpers === 'function' || isObject(helpers)) {
-    var keys = Object.keys(helpers);
-    for (var i = 0; i < keys.length; i++) {
-      var name = keys[i];
-      if (name !== 'async' && name !== 'sync' && name !== 'displayName') {
-        return true;
-      }
-    }
+    var keys = Object.keys(helpers).filter(function(name) {
+      return ['async', 'sync', 'displayName'].indexOf(name) === -1;
+    });
+    return keys.length > 1;
   }
   return false;
 }
